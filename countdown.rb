8ed753c713dccf0faf1095cfaa29c1fb7bceb161
Amedeo d'Amore <amedeodamore@gmail.com>
2020-11-20 00:47:24 +0000

    commit tree c96fcd71e56f9184041011b7dea0b777c6b35fef

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,43 @@
 #write your code here
 
-def countdown
+def countdown(seconds_to_midnight)
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECOND(S)!"
+        seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECOND(S)!"
+        sleep(5)
+        seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+
+
+
+#def countdown
+#end
+
+#sec = 10
+
+#def countdown_with_sleep(sec)
+ #   while sec > 0
+  #  puts "#{sec} SECOND(S)!"
+   # sec += 1
+ #   end
+  #  "HAPPY NEW YEAR!"
+ # end
+ # def countdown_with_sleep(sec)
+  # while sec > 0
+   # puts "#{sec} SECOND(S)!"
+    #sleep(1)
+   # sec -= 1
+   # end
+   # "HAPPY NEW YEAR!"
+  #end
